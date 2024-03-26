fbbf6f02748e5c9fcbcafa6db7367857d1d4f4d3
prelivemoduleserver <prelivemoduleserver@viennasolutions.com>
2024-03-26 09:38:01 +0000

    Add project files.

---
+++ ViennaAdvantage/Areas/VA106/Scripts/model/callouts.js
@@ -0,0 +1,53 @@
+﻿/*Module Name space initialization*/
+; VA106 = window.VA106 || {};
+
+; (function (VA106, $) {
+
+    var Level = VIS.Logging.Level;
+    var Util = VIS.Utility.Util;
+
+
+
+    /**
+    *  Callout Class
+      -   must call this function
+             VIS.CalloutEngine.call(this, [className]);
+    */
+    function VA106_CalloutTest() {
+        VIS.CalloutEngine.call(this, "VA106.VA106_CalloutTest"); // must call base class (CalloutEngine)
+    };
+
+
+    VA106_CalloutTest.prototype.setTotalAmt = function (ctx, windowNo, mTab, mField, value, oldValue) {
+        if (this.isCalloutActive() || value == null) {
+            return "";
+        }
+        this.setCalloutActive(true);
+        try { }
+        catch (err) {
+            this.log.severe(err.toString());
+            this.setCalloutActive(false);
+            return err.message;
+        };
+        this.setCalloutActive(false);
+        ctx = windowNo = mTab = mField = value = oldvalue = null;
+        return "";
+    };
+
+    VA106.Model = VA106.Model || {};
+    VA106.Model.VA106_CalloutTest = VA106_CalloutTest; //assign object in Model NameSpace
+
+
+
+})(VA106, jQuery);
+
+
+
+
+
+
+
+
+
+
+
---
+++ ViennaAdvantage/Areas/VA106/VA106AreaRegistration.cs
@@ -0,0 +1,61 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+
+
+
+//NOTE:--    Please replace ViennaAdvantage with prefix of your module..
+
+
+
+namespace VA106 //  Please replace namespace with prefix of your module..
+{
+    public class ViennaAdvantageAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "VA106";   //Please replace "ViennaAdvantage" with prefix of your module.......
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "VA106_default",
+                "VA106/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+                , new[] { "VA106.Controllers" }
+            );    // Please replace ViennaAdvantage with prefix of your module...
+
+
+            StyleBundle style = new StyleBundle("~/Areas/VA106/Contents/VA106Style");
+           
+            ScriptBundle script = new ScriptBundle("~/Areas/VA106/Scripts/VA106Js");
+
+
+            //script.Include("~/Areas/VA106/Scripts/model/callouts.js");
+
+            script.Include("~/Areas/VA106/Scripts/VA106.all.min.js");
+            style.Include("~/Areas/VA106/Contents/VA106.all.min.css");
+
+
+
+            /*-------------------------------------------------------
+                    Here include all js files in style bundle......see example below....
+            
+              Please replace "ViennaAdvantage" with prefix of your module..
+             * 
+             * 1. first parameter is script/style bundle...
+             * 
+             * 2. Second parameter is module prefix...
+             * 
+             * 3. Third parameter is order of loading... (dafault is 10 )
+             * 
+             --------------------------------------------------------*/
+
+            VAdvantage.ModuleBundles.RegisterScriptBundle(script, "VA106", 10);
+            VAdvantage.ModuleBundles.RegisterStyleBundle(style, "VA106", 10);
+        }
+    }
+}
